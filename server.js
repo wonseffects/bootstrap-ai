e34b88d0cbfd28f374c0f4b260f1fdbf4ba2da98
wonseffects <123042079+wonseffects@users.noreply.github.com>
2026-02-10 00:06:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = 'path';
+const path = require('path');
 const chatRoutes = require('./src/routes/chat');
 
 const app = express();
